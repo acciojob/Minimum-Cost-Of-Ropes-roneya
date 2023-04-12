7db06cda17299ce858e80029686c5632af499b3d
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-12 12:36:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function calculateMinCost() {  
-    var res = 0;  
-      var cost = 0;
-  var inputArray = document.getElementById("rope-lengths").value;
+function calculateMinCost() {
+  //your code here
+  // read data from input element;
+  var inputData = document.querySelector("#rope-lengths").value;
+  var inputArr = inputData.split(",");
 
-  inputArray = inputArray.split(',').map(Number);
-  console.log(inputArray);
-  for (let i = 0; i < inputArray.length - 1; i += 0) {
-      if (inputArray.length == 0) break;
-      inputArray = inputArray.sort((a,b) => b-a);
-      res = inputArray.pop() + inputArray.pop();
-      cost += res;
-      inputArray[inputArray.length] = res; }
-  document.getElementById("result").innerHTML = cost;
+  for (var i = 0; i < inputArr.length; i++) {
+    inputArr[i] = Number(inputArr[i]);
+  }
+  var cost = 0;
+
+  while (inputArr.length > 1) {
+    var newRope = inputArr[0] + inputArr[1];
+    cost += newRope;
+
+    // delete first two element
+    inputArr.splice(0, 2);
+    inputArr.push(newRope);
+    //alert(newRope);
+  }
+  document.querySelector("#result").textContent = cost;
 }
+
