27f5490dbe49d314fb05d6ea58448d02bf4992e5
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-12 12:25:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+function CalculateMinCost(){
+	var inputData= Document querySelector("#repo-lengths");
+	var inputArr= inputData.split(",");
+	
+	for(var i=0;i<inputArr.length;i++){
+		inputArr[i]=Number(inputArr[i]);
+	}
+	var cost=0;
+	inputArr.sort(function(a,b) {return a-b});
+	while (inputArr.length>1) {
+		var newRepo=inputArr[0]+inputArr[1];
+		cost+=newRope;
+		inputArr.splice(0,2);
+		inputArr.push(newRope);
+		inputArr.sort(function(a,b){return a-b});
+		
+	}
+	document.querySelector(#result).Content=cost;
+	})
